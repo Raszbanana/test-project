5ee049f7874e3307adc37f067fab48de0bafb9da
Raszbanana <niko.hochloff@gmail.com>
2022-05-10 10:57:32 +0200

    feature: added routes for signup and signin

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service'
+
+@Controller()
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post('signup')
+    signup() {
+        return 'i am signed up'
+
+    }
+    @Post('signin')
+    signin() {
+        return 'i am signed in'
+    }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller'
+import { AuthService } from './auth.service';
 
 @Module({
     imports: [],
-    controllers: [],
-    providers: [],
+    controllers: [AuthController],
+    providers: [AuthService],
 })
 export class AuthModule {}
