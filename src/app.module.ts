e53ca0ebaa92f72d0b0829897cea98cd68f50e47
Raszbanana <75260673+Raszbanana@users.noreply.github.com>
2022-05-18 14:29:44 +0200

    security: added json webtoken

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,12 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
-
 @Module({
-  imports: [UserModule, BookmarkModule, AuthModule, PrismaModule],
+  imports: [
+    UserModule,
+    BookmarkModule,
+    AuthModule,
+    PrismaModule
+  ],
 })
 export class AppModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,18 @@ import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    ) {}
+
   async signup(dto: AuthDto) {
     const hash = await argon.hash(dto.password);
+    
     try {
       const user = await this.prisma.user.create({
         data: {
@@ -17,7 +23,8 @@ export class AuthService {
         },
       });
       delete user.hash;
-      return user;
+      return this.signToken(user.id, user.email);
+
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
@@ -50,6 +57,21 @@ export class AuthService {
        );
        
     delete user.hash;
-    return user;
+    return this.signToken(user.id, user.email);
   }
+
+  signToken(
+    userId: number,
+    email: string
+    ): Promise<string> {
+      const payload = {
+        sub: userId,
+        email
+      }
+
+      return this.jwt.signAsync(payload, {
+        expiresIn: '15m',
+        secret: process.env.JWT_SECRET
+      })
+      }
 }   
