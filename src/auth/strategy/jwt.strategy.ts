4874c04ea6ec8f11e8e5af90df48845d9b5b6ceb
Raszbanana <75260673+Raszbanana@users.noreply.github.com>
2022-05-18 17:46:34 +0200

    security: added guards

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,11 +1,23 @@
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
+import { PrismaService } from "../../prisma/prisma.service";
 
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-    constructor() {
+    constructor(private prisma: PrismaService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             secretOrKey: process.env.JWT_SECRET,
-        })
+        });
     }
+
+    async validate(payload: {sub: number, email: string}) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: payload.sub
+            }
+        })
+        delete user.hash;
+        return null;
+    }
+    
 }
