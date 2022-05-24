95f2aba492636d34cc6eb64d6e4f43f0f6a34ac2
Raszbanana <75260673+Raszbanana@users.noreply.github.com>
2022-05-24 07:15:02 +0200

    feature: custom get-user decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express'
 import { JwtGuard } from 'src/auth/guard';
-
+import { GetUser } from '../auth/decorator/index'
 @Controller('users')
 export class UserController {
     @UseGuards(JwtGuard)
     @Get('me')
-    getMe(@Req() req: Request) {
+    getMe(@GetUser() req: Request) {
         return req.user;
     }
 }
